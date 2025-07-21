b6724e0a89402fab9d363af4df8259e52e11308a
kharijarrett <khari@viam.com>
2025-07-21 13:54:48 -0400

    RSDK-11357 - Vision Service for Keras Detector (#2)
    * first commit
    
    * move to our CI
    
    * make it function
    
    * add detector logic
    
    * add CAFC logic
    
    * fix detector
    
    * requested changes

---
+++ Makefile
@@ -0,0 +1,8 @@
+setup:
+	./setup.sh
+
+build:
+	./build.sh
+
+lint:
+	pylint --disable=C0301,C0303,C0116,C0103,R0913,W0201,C0114,C0115 src/
---
+++ src/main.py
@@ -0,0 +1,34 @@
+import asyncio
+from viam.services.vision import Vision
+from viam.module.module import Module
+from viam.resource.registry import Registry, ResourceCreatorRegistration
+from viam.errors import DuplicateResourceError
+try:
+    from src.keras_detector import KerasDetector
+except ModuleNotFoundError:
+    from keras_detector import KerasDetector
+
+
+async def main():
+    """
+    This function creates and starts a new module, after adding all desired
+    resource models. Resource creators must be registered to the resource
+    registry before the module adds the resource model.
+    """
+    Registry.register_resource_creator(
+        Vision.API,
+        KerasDetector.MODEL,
+        ResourceCreatorRegistration(
+            KerasDetector.new, KerasDetector.validate_config
+        ),
+    )
+    module = Module.from_args()
+    module.add_model_from_registry(Vision.API, KerasDetector.MODEL)
+    await module.start()
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except DuplicateResourceError:
+        asyncio.run(Module.run_from_registry())
+    
