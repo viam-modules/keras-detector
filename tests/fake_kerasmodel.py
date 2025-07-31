3ced9459144580f180d8c9341c838dbb8e5f243d
Khari Jarrett <khari@viam.com>
2025-07-31 11:18:01 -0400

    fix spacing

--- tests/fake_kerasmodel.py
+++ tests/fake_kerasmodel.py
@@ -3,7 +3,6 @@ class FakeKerasModel():
     def __init__(self, name: str):
         self.name = name
 
-        
     def predict(self, np_array, verbose: int = 0):
         # Simulate a prediction output
         return [[10, 20, 30, 40]] 
